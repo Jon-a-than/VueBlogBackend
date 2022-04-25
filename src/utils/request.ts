479f453e1d297e39aa60e19151bf96c66ee4afd8
Jon-a-than <2140145186@qq.com>
2022-04-25 22:36:18 +0800

    feat: packaging axios.post

---
+++ src/utils/request.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+// const ApiGet = (url: string, params: any) => {
+//   return axios.get(url, {
+//     params,
+//     ...config
+//   })
+// }
+
+const ApiPost = (url: string, data: any, config?: any) => {
+  return axios.post(url, data, config)
+}
+
+
+export { ApiPost }
